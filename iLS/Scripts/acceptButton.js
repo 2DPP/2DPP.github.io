240804d9c8e46ef251f52617ff42086a06a6108a
Twoods <114866493+2DPP@users.noreply.github.com>
2024-05-21 04:17:38 +0800

    Update acceptButton.js

--- iLS/Scripts/acceptButton.js
+++ iLS/Scripts/acceptButton.js
@@ -1,10 +1,11 @@
 // acceptButton.js
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('acceptButton').addEventListener('click', function(event) {
         // Add a class to body to indicate terms have been accepted
         document.body.classList.add('terms-accepted');
         // Hide the terms and conditions div
         document.getElementById('termsOfServiceContainer').style.display = 'none';
+        // Scroll to the top of the page
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
